2054bbb6964d581842c07902804e03e977541aba
Viet Bui <vietbh@beetsoft.com.vn>
2022-10-14 14:56:36 +0700

    feat(socket): update api to socket

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 const messageRoutes = require("./routes/messages");
 const app = express();
-const socket = require("socket.io");
+const http = require('http');
+const {Server} = require("socket.io");
+const server = http.createServer(app);
+const io = new Server(server);
 const device = require('express-device');
+const {logOut} = require("./controllers/userController");
 require("dotenv").config();
 
 app.use(cors());
@@ -13,41 +17,47 @@ app.use(express.json());
 app.use(device.capture());
 
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("DB Connetion Successfull");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("DB Connetion Successfull");
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const server = app.listen(process.env.PORT, () =>
-  console.log(`Server started on ${process.env.PORT}`)
-);
-const io = socket(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    credentials: true,
-  },
+io.on('connection', (socket) => {
+    socket.on('logout all device', (refreshToken) => {
+        logOut(
+            {
+                path: '/logout-all',
+                body: {
+                    refreshToken: refreshToken
+                }
+            }
+        ).then(() => {
+            io.emit('devices logout', 'devices logout');
+        });
+    })
+    socket.on('logout device', (refreshToken) => {
+        logOut(
+            {
+                path: '/logout',
+                body: {
+                    refreshToken: refreshToken
+                }
+            }
+        ).then(() => {
+            io.emit('device logout', 'device logout');
+        });
+    })
 });
 
-global.onlineUsers = new Map();
-io.on("connection", (socket) => {
-  global.chatSocket = socket;
-  socket.on("add-user", (userId) => {
-    onlineUsers.set(userId, socket.id);
-  });
-
-  socket.on("send-msg", (data) => {
-    const sendUserSocket = onlineUsers.get(data.to);
-    if (sendUserSocket) {
-      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
-    }
-  });
+server.listen(5050, () => {
+    console.log('listening on *:5050');
 });
